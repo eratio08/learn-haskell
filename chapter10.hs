83ea17e0517b568afa5bca26a0f4d0764d5610dd
eratio <eratio@rat.fritz.box>
2017-08-23 10:41:21 +0200

    Finishes Chapter 10, begins Chapter 11

--- chapter10.hs
+++ chapter10.hs
@@ -26,3 +26,56 @@ sumDb = foldr (+) 0 . filterDbNumber
 
 avgDb :: [DatabaseItem] -> Double
 avgDb xs = (fromIntegral . sumDb $ xs) / (fromIntegral . length . filterDbNumber $ xs)
+
+fibs = 1 : scanl (+) 1 fibs
+fibsN x = fibs !! x
+fac = 1 : scanl (\x y -> x*y) 1 fac
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+makeComb :: String -> String -> [(Char, Char, Char)]
+makeComb stops vowels = [ (x,y,z) | x <- stops, y <- vowels, z <- stops, x == 'p' ]
+
+whitespace :: String -> Int
+whitespace x = div (sum (map length (words x))) (length (words x))
+
+-- Rewirting functions using folds
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f = foldr ((||) . f) False
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem x = foldr (\e acc -> acc || (e == x)) False
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' x = any (\e -> x == e)
+
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr ((:) . f) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\x acc -> if f x then x : acc else acc) []
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr ((++) . f) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap ([]++)
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = foldr (\e acc -> case f e cc of
+                                          GT -> e
+                                          EQ -> acc
+                                          LT ->acc ) x (x:xs)
